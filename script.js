1a51bf5758f9723916b6ccbbdedfbcfacefa13dd
AliSenina <122289718+AliSenina@users.noreply.github.com>
2023-01-31 20:26:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let city = 'Кyiv'
+city = 'Lviv'
+
+let adress = city
+alert(adress)
+
+
+
+let n = 3
+let result = n * n * n
+console.log(result)
+
+let pen=4
+let pencil=6
+let pricePen=5.25
+let pricePencil=3.50
+let count= (pen*pricePen)+(pencil*pricePencil)
+document.write(count)
